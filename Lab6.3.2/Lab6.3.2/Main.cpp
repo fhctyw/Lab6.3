39b9d93a0e5cd748fe902e2aa14e7192195ce57e
fhctyw <lisnychukarsenw@gmail.com>
2021-11-02 23:44:13 +0200

    add rec template

--- Lab6.3.2/Lab6.3.2/Main.cpp
+++ Lab6.3.2/Lab6.3.2/Main.cpp
@@ -16,7 +16,34 @@ void InitArray(int arr[], const int size, int i)
 	}
 }
 
+template<typename T>
+void InitArray(T arr[], const size_t size, size_t i)
+{
+	if (i < size)
+	{
+		arr[i] = (T)(-10 + rand() % 21);
+		InitArray(arr, size, i + 1);
+		return;
+	}
+}
+
 void PrintArray(const int* const arr, const int size, int i)
+{
+	if (i < size)
+	{
+		if (i == 0)
+			cout << "{";
+		cout << arr[i];
+		if (i != size - 1)
+			cout << ", ";
+		else cout << "}" << endl;
+		PrintArray(arr, size, i + 1);
+		return;
+	}
+}
+
+template<typename T>
+void PrintArray(const T* const arr, const size_t size, size_t i)
 {
 	if (i < size)
 	{
@@ -46,6 +73,22 @@ int Min(int arr[], int size, int &index, int i)
 	else return numeric_limits<int>::max();
 }
 
+template<typename T>
+int Min(const T const arr[], size_t size, size_t& index, size_t i)
+{
+	index = 0;
+	if (i < size)
+	{
+		if (arr[i] < Min(arr, size, index, i + 1)) {
+			index = i;
+			return arr[i];
+		}
+		else
+			return Min(arr, size, index, i + 1);
+	}
+	else return numeric_limits<T>::max();
+}
+
 void SortArray(int arr[], const int size, int i)
 {
 	if (i < size)
@@ -60,6 +103,21 @@ void SortArray(int arr[], const int size, int i)
 
 }
 
+template<typename T>
+void SortArray(T arr[], const size_t size, size_t i)
+{
+	if (i < size)
+	{
+		size_t index;
+		size_t m = Min(arr, size, index, i);
+		arr[index] = arr[i];
+		arr[i] = m;
+		SortArray(arr, size, i + 1);
+		return;
+	}
+
+}
+
 int main()
 {
 	srand(time(0));
@@ -67,10 +125,10 @@ int main()
 	cout << "n = "; cin >> n;
 	int* arr = new int[n];
 
-	InitArray(arr, n, 0);
-	PrintArray(arr, n, 0);
-	SortArray(arr, n, 0);
-	PrintArray(arr, n, 0);
+	InitArray<int>(arr, n, 0);
+	PrintArray<int>(arr, n, 0);
+	SortArray<int>(arr, n, 0);
+	PrintArray<int>(arr, n, 0);
 
 	delete[] arr;
 	arr = nullptr;
